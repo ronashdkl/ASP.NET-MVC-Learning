00b1209b2f06853fcd62290fefd28666c67c8452
Ronash Dhakal <ronashmail@gmail.com>
2017-02-01 14:34:29 +0545

    Playing with Authentication and custom roles

---
+++ LearnMVC/ViewModel/Auth.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LearnMVC.ViewModel
+{
+    public class Auth
+    {
+        [Required]
+        public string username { get; set; }
+        [DataType(DataType.Password), Required]
+        public string password { get; set; }
+
+    }
+}
---
+++ LearnMVC/ViewModels/Auth.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LearnMVC.ViewModels
+{
+    public class Auth
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+
+    }
+}
