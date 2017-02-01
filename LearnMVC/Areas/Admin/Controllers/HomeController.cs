d68a8c3eb6388031a0613c7b546b88863567ee12
Ronash Dhakal <ronashmail@gmail.com>
2017-02-01 20:52:10 +0545

    Layout

--- LearnMVC/Areas/Admin/Controllers/HomeController.cs
+++ LearnMVC/Areas/Admin/Controllers/HomeController.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LearnMVC.Infrastructure;
 
 namespace LearnMVC.Areas.Admin.Controllers
 {
     [Authorize(Roles ="admin")]
+    [SelectedTab("Home")]
     public class HomeController : Controller
     {
         // GET: Admin/Home
--- LearnMVC/Controllers/HomeController.cs
+++ LearnMVC/Controllers/HomeController.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LearnMVC.Infrastructure;
 namespace LearnMVC.Controllers
 {
+    [SelectedTab("Home")]
     public class HomeController : Controller
     {
         [HttpGet]
